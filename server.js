19e67c9bb5888fc3cf5968c0ece8ee1de8572f31
Edu Casal <eduardo.fernandez.casal@gmail.com>
2018-02-22 17:23:33 +0100

    Serve static files

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 const Boom = require('boom');
 const Good = require('good');
 const Hapi = require('hapi');
+const Inert = require('inert');
+const Path = require('path');
 
 const server = new Hapi.Server();
 server.connection({ port: 8000, host: 'localhost' });
 
-let registerOptions = {
+let goodOptions = {
   reporters: {
     console: [
       {
@@ -18,10 +20,10 @@ let registerOptions = {
   }
 };
 
-server.register({
-  register: Good,
-  options: registerOptions
-}, (err) => {
+server.register([
+  { register: Good, options: goodOptions },
+  { register: Inert }
+], (err) => {
   if (err) {
     throw err;
   }
@@ -30,25 +32,41 @@ server.register({
     method: 'GET',
     path: '/{splat*}',
     handler: (request, reply) => {
-      server.log('info', 'Asking for /');
+      server.log('error', 'Asking for /');
       reply(Boom.notFound());
     }
   });
 
   server.route({
-      method: 'GET',
-      path: '/users/{name}',
-      handler: function (request, reply) {
-          reply(`Hello, ${encodeURIComponent(request.params.name)}!`);
-      }
+    method: 'GET',
+    path: '/users/{name}',
+    handler: function (request, reply) {
+      reply(`Hello, ${encodeURIComponent(request.params.name)}!`)
+      .type('text/plain')
+      .code(418);;
+    }
   });
 
+  //Specific route for images, always the same one
   server.route({
-      method: 'GET',
-      path: '/files/{file}.jpg',
-      handler: function (request, reply) {
-          reply(Promise.resolve(`Asking for JPG: ${encodeURIComponent(request.params.file)}!`));
+    method: 'GET',
+    path: '/images/{image}.jpg',
+    handler: function (request, reply) {
+      server.log('error', `Redirecting to default JPG instead of ${encodeURIComponent(request.params.image)}!`);
+      const path = Path.join(__dirname, 'public/images/waterme.jpg')
+      reply.file(path);
+    }
+  });
+
+  //Generic routing to a directory
+  server.route({
+    method: 'GET',
+    path: '/files/{file*}',
+    handler: {
+      directory: {
+        path: Path.join(__dirname, 'public')
       }
+    }
   });
 
   server.start((err) => {
